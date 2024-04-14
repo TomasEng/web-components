dc12fccd50f7be773aef0942589b83592fc8765c
Tomas <tomas.engebretsen@gmail.com>
2024-04-14 14:15:28 +0200

    Om-side

--- packages/stencil/src/preview-app/page-about/page-about.tsx
+++ packages/stencil/src/preview-app/page-about/page-about.tsx
@@ -8,7 +8,10 @@ export class PageAbout {
     return (
       <>
         <t-heading level={1}>Om</t-heading>
-        <p>Lorem ipsum dolor sit amet</p>
+        <p>
+          Dette er et nettsideutformingsverktøy som i utgangspunktet er laget for mine personlige prosjekter, men jeg er bare glad til om det er til nytte for noen andre.
+          Komponentene er bygget som webkomponenter ved hjelp av verktøyet Stencil, men de er også tilgjengelige som React-komponenter.
+        </p>
       </>
     );
   }
