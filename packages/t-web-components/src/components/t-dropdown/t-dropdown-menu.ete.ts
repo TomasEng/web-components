18cdf17cd4f678a52ddf88c8cac777fc439fdf44
Tomas <tomas.engebretsen@gmail.com>
2024-05-09 10:57:15 +0200

    Legger til tester for nedtrekksmeny

--- packages/t-web-components/src/components/t-dropdown/t-dropdown-menu.ete.ts
+++ packages/t-web-components/src/components/t-dropdown/t-dropdown-menu.ete.ts
@@ -1,21 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 import { renderComponent } from '../../test-utils/renderComponent';
 
-test.describe('t-dropdown-menu', () => {
-  test('Laster nedtrekksmeny', async ({ page }) => {
-    const consoleLog: string[] = [];
-    page.on('console', msg => consoleLog.push(msg.text()));
-    const buttonLabel = 'Velg';
-    const html = `<t-dropdown-menu label="${buttonLabel}"></t-dropdown-menu>`;
 
-    const logValues: { [key: string]: string } = {
-      rod: 'Rød',
-      bla: 'Blå',
-      gronn: 'Grønn valgt',
-    };
-    const filterLog = (log: string[]) => log.filter((msg) => Object.values(logValues).includes(msg));
+const buttonLabel = 'Velg';
+const html = `<t-dropdown-menu label="${buttonLabel}"></t-dropdown-menu>`;
 
-    const script = `
+const logValues: { [key: string]: string } = {
+  rod: 'Rød',
+  bla: 'Blå',
+  gronn: 'Grønn valgt',
+};
+
+const script = `
       const dropdown = document.querySelector('t-dropdown-menu');
       dropdown.items = [
         {label: 'Rød', action: () => console.log('${logValues.rod}')},
@@ -24,7 +20,17 @@ test.describe('t-dropdown-menu', () => {
       ];
     `.replace(/\n/g, '');
 
-    await renderComponent(page, html, script);
+const filterLog = (log: string[]) => log.filter((msg) => Object.values(logValues).includes(msg));
+
+const renderDropdown = (page: Page) => renderComponent(page, html, script);
+
+test.describe('t-dropdown-menu', () => {
+  test('Laster nedtrekksmeny', async ({ page }) => {
+    const consoleLog: string[] = [];
+    page.on('console', msg => consoleLog.push(msg.text()));
+
+    await renderDropdown(page);
+    await expect(page.getByRole('list')).not.toBeVisible();
     const dropdown = page.getByRole('button', { name: buttonLabel });
     await dropdown.click();
     const menu = page.getByRole('list');
@@ -33,5 +39,58 @@ test.describe('t-dropdown-menu', () => {
     const redButton = page.getByRole('button', { name: 'Rød' });
     await redButton.click();
     expect(filterLog(consoleLog)).toEqual([logValues.rod]);
+    await expect(page.getByRole('list')).not.toBeVisible();
+  });
+
+  test('Er navigerbar med piltaster', async ({ page }) => {
+    const consoleLog: string[] = [];
+    page.on('console', msg => consoleLog.push(msg.text()));
+    await renderDropdown(page);
+    const dropdown = page.getByRole('button', { name: buttonLabel });
+    await dropdown.press('Enter');
+    const menu = page.getByRole('list');
+    await expect(menu).toBeVisible();
+    expect(filterLog(consoleLog)).toEqual([]);
+    await menu.press('ArrowDown');
+    await menu.press('ArrowDown');
+    await menu.press('Enter');
+    expect(filterLog(consoleLog)).toEqual([logValues.bla]);
+    await expect(page.getByRole('list')).not.toBeVisible();
+  });
+
+  test('Lukker meny ved klikk på Escape-knappen', async ({ page }) => {
+    await renderDropdown(page);
+    const dropdown = page.getByRole('button', { name: buttonLabel });
+    await dropdown.press('Enter');
+    const menu = page.getByRole('list');
+    await expect(menu).toBeVisible();
+    await page.keyboard.press('Escape');
+    await expect(menu).not.toBeVisible();
+  });
+
+  test('Lukker meny ved klikk utenfor', async ({ page }) => {
+    await renderDropdown(page);
+    const dropdown = page.getByRole('button', { name: buttonLabel });
+    await dropdown.press('Enter');
+    const menu = page.getByRole('list');
+    await expect(menu).toBeVisible();
+    await page.mouse.click(0, 0);
+    await expect(menu).not.toBeVisible();
+  });
+
+  test('Fokuserer på øverste element ved klikk på ned-knapp', async ({ page }) => {
+    await renderDropdown(page);
+    const dropdown = page.getByRole('button', { name: buttonLabel });
+    await dropdown.press('ArrowDown');
+    const firstItem = page.getByRole('button', { name: 'Rød' });
+    await expect(firstItem).toBeFocused();
+  });
+
+  test('Fokuserer på nederste element ved klikk på opp-knapp', async ({ page }) => {
+    await renderDropdown(page);
+    const dropdown = page.getByRole('button', { name: buttonLabel });
+    await dropdown.press('ArrowUp');
+    const lastItem = page.getByRole('button', { name: 'Grønn' });
+    await expect(lastItem).toBeFocused();
   });
 });
